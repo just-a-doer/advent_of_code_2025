c14519a5ea91008958a4d4582a3f074e9ec26cd3
lpollard6 <pollard_c@bellsouth.net>
2025-12-09 19:31:43 -0600

    more changes

--- Day_1/Part_2.py
+++ Day_1/Part_2.py
@@ -1,5 +1,3 @@
-array_1 =[]
-array_2 = []
 
 def check_sign(number):
     if number >= 0:
@@ -7,38 +5,33 @@ def check_sign(number):
     elif number < 0:
         return False
 
+num_zeros = 0
 
 orig_dial_loc = 50
-num_zeros = 0
 with open("input.txt", "r") as f:
     for line in f:
-        if line[0] == "L":
-            direction = -1
-        elif line[0] == "R":
-            direction = 1
         dist_str = line[1:]
-        dist_val = int(dist_str)
-        num_rotations = int(dist_val/100)
-        dist = direction * (int(dist_str) % 100)
-        dial_loc = orig_dial_loc + dist
+        dist = int(dist_str)
+        if line[0] == "L":
+            dial_loc = orig_dial_loc - dist
+        elif line[0] == "R":
+            dial_loc = orig_dial_loc + dist
 
-        sign_loc = check_sign(dial_loc)
-        sign_orig_loc = check_sign(orig_dial_loc)
-
-        if sign_loc != sign_orig_loc and orig_dial_loc != 0:
+        num_revolutions = abs(int(dist/100))
+        if (dial_loc < 0 and orig_dial_loc != 0) or dial_loc > 100:
             num_zeros += 1
         elif dial_loc == 0:
             num_zeros += 1
+        elif dial_loc == 100:
+            num_zeros += 1
+        if num_revolutions > 1:
+            num_zeros += num_revolutions
+        orig_dial_loc = dial_loc % 100
 
-        if dial_loc >= 100:
-            num_rotations = int(dial_loc/100)
-            dial_loc -= 100
-        elif dial_loc < 0:
-            dial_loc += 100
-        print(dial_loc)
-        if num_rotations > 0:
-            num_zeros += num_rotations
-        orig_dial_loc = dial_loc
+
     print(num_zeros)
+
+
+
 
 
