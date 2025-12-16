1f3b06ba9801159d930a68f8cf11ee7d26800c81
lpollard6 <chris.pollard@gtri.gatech.edu>
2025-12-15 21:19:40 -0600

    Finished Day 7 Part 1

--- Day_7/Part_1.py
+++ Day_7/Part_1.py
@@ -1,7 +1,25 @@
 
 with open("input.txt", "r") as f:
+    lines = f.readlines()
+    beams = set([lines[0].find('S')])
+    grid_width = len(lines[0])
+    grid_height = len(lines)
 
-    print(split_count)
+    num_splits = 0
+    for line in lines:
+        current_beams = beams.copy()
+        for beam in current_beams:
+            if line[beam] == '^':
+                num_splits += 1
+                beams.remove(beam)
+                right_beam = beam + 1
+                left_beam = beam - 1
+                if right_beam < grid_width:
+                    beams.add(right_beam)
+                if left_beam >= 0:
+                    beams.add(left_beam)
+
+    print(num_splits)
 
 
 
