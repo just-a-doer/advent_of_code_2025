12d7e55acd0a4bfb88d549bf88a99d83256e0910
lpollard6 <pollard_c@bellsouth.net>
2025-12-17 11:43:46 -0600

    working on day 7 part 2

--- Day_7/Part_2.py
+++ Day_7/Part_2.py
@@ -1,25 +1,51 @@
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
-    beams = set([lines[0].find('S')])
+    start_beam  = lines[0].find('S')
     grid_width = len(lines[0])
     grid_height = len(lines)
 
-    num_beams = 0
-    for line in lines:
-        current_beams = beams.copy()
-        for beam in current_beams:
+    path_lists = [start_beam]
+    new_beam_lists = []
+    path_set = set(path_lists)
+    duplicates = 0
+    for index in range(1,len(lines)):
+        print(index)
+        line = lines[index]
+        for path in path_lists:
+            beam = path
             if line[beam] == '^':
-                num_beams += 2
-                beams.remove(beam)
                 right_beam = beam + 1
                 left_beam = beam - 1
                 if right_beam < grid_width:
-                    beams.add(right_beam)
+                    new_beam_lists.append(right_beam)
                 if left_beam >= 0:
-                    beams.add(left_beam)
+                    new_beam_lists.append(left_beam)
+            else:
+                new_beam_lists.append(beam)
+        path_lists = new_beam_lists.copy()
+        path_set = set(path_lists)
+        duplicates += len(path_lists)-len(path_set)
+        new_beam_lists.clear()
 
-    print(num_beams)
+print(len(path_lists))
+print(path_lists)
+path_set = set(path_lists)
+print(path_set)
+# final_path_list = [path_lists[0]]
+# j = 0
+# for path in path_lists:
+#     j += 1
+#     print(j)
+#     for i in range(0,len(final_path_list)):
+#         if path != final_path_list[i]:
+#             final_path_list.append(path)
+
+
+
+
+
+
 
 
 
